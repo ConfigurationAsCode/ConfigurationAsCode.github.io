0f34d304cb7dc40b2f6bfece40f0c3f1002e74ae
Thomas William <thomas.william@citrix.com>
2016-01-09 01:43:34 +0100

    Fix missing "

--- make.sh
+++ make.sh
@@ -6,5 +6,7 @@ rst2html5slides --traceback --strict --tab-width=4\
             --template templates/jmpress.html \
                         slides.rst index.html
 
+ls -la index.html
+
 rm -f slides.rst
 
